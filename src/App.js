9e5b5f412814845b4d803d49350fc8a6b04a69df
Nick Lils <nicklils@Nicks-Mac-mini.local>
2019-03-09 01:24:36 +0000

    cors update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axio from 'axios';
+import axios from 'axios';
 import './App.css';
 import DjList from './components/DjList';
 import Nav from './components/Nav';
