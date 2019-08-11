9f26f057ade6366c29d5b1f3b5cc9e3577897d76
Your Name <nickliliakides@gmail.com>
2019-08-11 03:22:08 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.myjson.com/bins/15nbcb')
+    fetch('https://api.myjson.com/bins/19a9uj')
       .then(res => res.json())
       .then(djs => this.setState({ djs }));
   }
